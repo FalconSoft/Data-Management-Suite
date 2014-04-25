01d797089356548b9691340b8317633ebc9547ff
Taras Ilkiv <tilkiv@falconsoft-ltd.com>
2014-04-25 18:02:54 +0300

    new meta RelationInfo

--- Src/Common/Metadata/RelationshipInfo.cs
+++ Src/Common/Metadata/RelationshipInfo.cs
@@ -5,46 +5,32 @@ namespace FalconSoft.ReactiveWorksheets.Common.Metadata
     /// <summary>
     /// Represents a DataSource relationship between two data sources
     /// </summary>
-    public class BaseRelationshipInfo 
-    {
+    public class RelationshipInfo
+    { 
+        /// <summary>
+        /// Relation Type
+        /// </summary>
+        public RelationType RelationType { get; set; }
+        /// <summary>
+        /// Relation Name
+        /// </summary>
         public string Name { get; set; }
         /// <summary>
         /// Main SourceInfo DataSourcePath
         /// </summary>
         public string SourceInfoProviderString { get; set; }
-
         /// <summary>
         /// Related SourceInfo provider string to call data provider
         /// </summary>
         public string RelatedSourceInfoProviderString { get; set; }
-    }
-
-    /// <summary>
-    /// Represents a DataSource relationship between two data sources
-    /// </summary>
-    public class RelationshipInfo : BaseRelationshipInfo
-    {
-        /// <summary>
-        /// Foreign Field Id
-        /// a field from main datasource what suppose to reference another datasource
-        /// </summary>
-        public string ForeignFieldName { get; set; }
-
-        /// <summary>
-        /// Field from related datasource
-        /// Foreign Field from main data source will look up for primary field from related data source   
-        /// </summary>
-        public string PrimaryFieldName { get; set; }
-    }
-
-    /// <summary>
-    /// Represent relationships between two data sources based on number of input parameters
-    /// </summary>
-    public class MultiColumnRelations : BaseRelationshipInfo
-    {
         /// <summary>
         /// A mapping table for the fields from two data sources
         /// </summary>
-        public Dictionary<string, string> InputFieldsMapping { get; set; }
+        public Dictionary<string, string> MappedFields { get; set; }
+    }
+
+    public enum RelationType
+    {
+        Single,Multi
     }
 }
