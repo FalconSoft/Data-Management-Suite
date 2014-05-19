41a58eaab2e0e93a80d138a248f094d966c186b0
Volodymyr <vkalinchuk@falconsoft-ltd.com>
2014-05-19 18:24:40 +0300

    add time

--- Src/Server/Server.ConsoleRunner/Program.cs
+++ Src/Server/Server.ConsoleRunner/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Configuration;
-using System.Reflection;
 using FalconSoft.ReactiveWorksheets.Server.Bootstrapper;
 using FalconSoft.ReactiveWorksheets.Server.SignalR.Hubs;
 using Microsoft.AspNet.SignalR;
@@ -67,24 +66,24 @@ namespace ReactiveWorksheets.Server.ConsoleRunner
     {
         protected override void OnIncomingError(Exception ex, IHubIncomingInvokerContext context)
         {
-            Console.WriteLine("=> Invoking " + context.MethodDescriptor.Name + " on hub " + context.MethodDescriptor.Hub.Name);
+            Console.WriteLine("=>" + DateTime.Now.ToString("HH:mm:ss") + " Invoking " + context.MethodDescriptor.Name + " on hub " + context.MethodDescriptor.Hub.Name);
             base.OnIncomingError(ex, context);
         }
 
         protected override bool OnBeforeConnect(IHub hub)
         {
-            Console.WriteLine("=> OnBeforeConnect " + hub.Context.QueryString);
+            Console.WriteLine("=>" + DateTime.Now.ToString("HH:mm:ss") + " OnBeforeConnect " + hub.Context.QueryString);
             return base.OnBeforeConnect(hub);
         }
 
         protected override bool OnBeforeIncoming(IHubIncomingInvokerContext context)
         {
-            Console.WriteLine("=> Invoking " + context.MethodDescriptor.Name + " on hub " + context.MethodDescriptor.Hub.Name);
+            Console.WriteLine("=>" + DateTime.Now.ToString("HH:mm:ss") + " Invoking " + context.MethodDescriptor.Name + " on hub " + context.MethodDescriptor.Hub.Name);
             return base.OnBeforeIncoming(context);
         }
         protected override bool OnBeforeOutgoing(IHubOutgoingInvokerContext context)
         {
-            Console.WriteLine("<= Invoking " + context.Invocation.Method + " on client hub " + context.Invocation.Hub);
+            Console.WriteLine("<=" + DateTime.Now.ToString("HH:mm:ss") + " Invoking " + context.Invocation.Method + " on client hub " + context.Invocation.Hub);
             return base.OnBeforeOutgoing(context);
         }
     }
