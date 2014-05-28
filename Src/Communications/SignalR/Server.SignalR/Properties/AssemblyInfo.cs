21695a75eb56e6172b1366b88bf3791d1c36fad1
ppaska <ppaska@falconsoft-ltd.com>
2014-05-28 17:03:04 +0300

    added version info

--- Src/Communications/SignalR/Server.SignalR/Properties/AssemblyInfo.cs
+++ Src/Communications/SignalR/Server.SignalR/Properties/AssemblyInfo.cs
@@ -22,15 +22,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fe63adc8-295a-4fab-9f67-8db9a9b42dfb")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
