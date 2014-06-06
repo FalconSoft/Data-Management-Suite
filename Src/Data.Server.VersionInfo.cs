13ec6bd552989a614fed49c771f4721eb00117ee
Taras Ilkiv <tilkiv@falconsoft-ltd.com>
2014-06-06 13:47:33 +0300

    change version

--- Src/Data.Server.VersionInfo.cs
+++ Src/Data.Server.VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.3.*")]
-[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1")]
