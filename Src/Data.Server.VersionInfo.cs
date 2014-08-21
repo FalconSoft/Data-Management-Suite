aa069300e9df8edc79ae2af25e731f9e0bc2375b
Volodymyr <vkalinchuk@falconsoft-ltd.com>
2014-08-21 14:05:53 +0300

    Versions

--- Src/Data.Server.VersionInfo.cs
+++ Src/Data.Server.VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.1.*")]
-[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.6.*")]
+[assembly: AssemblyFileVersion("1.0.6")]
