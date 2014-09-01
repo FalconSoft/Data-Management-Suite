c21fc6a1d44a67133b3bac6d0da56a23033f923a
Volodymyr <vkalinchuk@falconsoft-ltd.com>
2014-09-01 17:46:46 +0300

    Logger

--- Src/Clients/Console.Client/Logger.cs
+++ Src/Clients/Console.Client/Logger.cs
@@ -1,83 +1,127 @@
 ﻿using System;
-using FalconSoft.Data.Management.Common;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
 
 namespace FalconSoft.Data.Console
 {
-    public class Logger : ILogger
+    public class Logger : Management.Common.ILogger
     {
-        private readonly log4net.ILog _log;
+        private readonly ILog _log;
 
         public Logger()
         {
-            log4net.Config.XmlConfigurator.Configure();
+            Setup();
+            _log = LogManager.GetLogger("Main Log");
+        }
 
-            _log = log4net.LogManager.GetLogger("Main Log");
+        public void Setup()
+        {
+            var hierarchy = (Hierarchy)LogManager.GetRepository();
+
+            var patternLayout = new PatternLayout { ConversionPattern = "%date [%thread] %-5level %logger - %message%newline" };
+            patternLayout.ActivateOptions();
+
+            var roller = new RollingFileAppender
+            {
+                AppendToFile = false,
+                File = "log-file.txt",
+                Layout = patternLayout,
+                MaxSizeRollBackups = 10,
+                MaximumFileSize = "10MB",
+                RollingStyle = RollingFileAppender.RollingMode.Size,
+                StaticLogFileName = true
+            };
+            roller.ActivateOptions();
+            hierarchy.Root.AddAppender(roller);
+
+            var console = new ColoredConsoleAppender
+            {
+                Layout = patternLayout,
+                Target = ColoredConsoleAppender.ConsoleOut
+            };
+            console.AddMapping(new ColoredConsoleAppender.LevelColors
+            {
+                Level = Level.Debug,
+                ForeColor = ColoredConsoleAppender.Colors.Cyan | ColoredConsoleAppender.Colors.HighIntensity
+            });
+            console.AddMapping(new ColoredConsoleAppender.LevelColors
+            {
+                Level = Level.Info,
+                ForeColor = ColoredConsoleAppender.Colors.Green
+            });
+            console.AddMapping(new ColoredConsoleAppender.LevelColors
+            {
+                Level = Level.Warn,
+                ForeColor = ColoredConsoleAppender.Colors.Purple | ColoredConsoleAppender.Colors.HighIntensity
+            });
+            console.AddMapping(new ColoredConsoleAppender.LevelColors
+            {
+                Level = Level.Error,
+                ForeColor = ColoredConsoleAppender.Colors.Red
+            });
+            console.ActivateOptions();
+            hierarchy.Root.AddAppender(console);
+
+            hierarchy.Root.Level = Level.Debug;
+            hierarchy.Configured = true;
         }
 
         public void Debug(string message)
         {
             _log.Debug(message);
-            System.Console.WriteLine(message);
         }
 
         public void Debug(string message, Exception exception)
         {
             _log.Debug(message, exception);
-            System.Console.WriteLine(message + string.Format("Exception : {0}", exception.Message));
         }
 
         public void DebugFormat(string format, params object[] args)
         {
             _log.DebugFormat(format, args);
-            System.Console.WriteLine(format, args);
         }
 
         public void Error(string message)
         {
             _log.Error(message);
-            System.Console.WriteLine(message);
         }
 
         public void Error(string message, Exception exception)
         {
             _log.Error(message, exception);
-            System.Console.WriteLine(message + string.Format("Exception : {0}", exception.Message));
         }
 
         public void ErrorFormat(string format, params object[] args)
         {
             _log.ErrorFormat(format, args);
-            System.Console.WriteLine(format, args);
         }
 
         public void Info(string message)
         {
             _log.Info(message);
-            System.Console.WriteLine(message);
         }
 
         public void InfoFormat(string format, params object[] args)
         {
             _log.InfoFormat(format, args);
-            System.Console.WriteLine(format, args);
         }
 
         public void Warn(object message)
         {
             _log.Warn(message);
-            System.Console.WriteLine(message);
         }
 
         public void Warn(object message, Exception exception)
         {
             _log.Warn(message, exception);
-            System.Console.WriteLine(message + string.Format("Exception : {0}", exception.Message));
         }
 
         public void WarnFormat(string format, params object[] args)
         {
             _log.WarnFormat(format, args);
-            System.Console.WriteLine(format, args);
         }
     }
 }
